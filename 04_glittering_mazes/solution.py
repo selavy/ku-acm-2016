baf79c3613716f5a0dc7089b98a5346215b63bff
Peter Lesslie <pclesslie@gmail.com>
2016-04-28 19:51:44 -0500

    Reduce line count :P

--- 04_glittering_mazes/solution.py
+++ 04_glittering_mazes/solution.py
@@ -4,82 +4,63 @@ import sys
 import copy
 from collections import namedtuple
 
-Move = namedtuple('Move', ['x', 'y', 'maze', 'gem', 'path'])
-
 if __name__ == '__main__':
+    Move = namedtuple('Move', ['x', 'y', 'maze', 'gem'])
     def is_gem(val):
-        return val == 'R' or val == 'E'
+        return val in ('R', 'E')
     def helper(prev):
         loc = prev.maze[prev.y][prev.x]
-        gem = loc if is_gem(loc) else prev.gem
         maze = copy.deepcopy(prev.maze)
         maze[prev.y][prev.x] = 'X'
-        path = copy.deepcopy(prev.path)
-        return loc, gem, maze, path
+        return loc, loc if is_gem(loc) else prev.gem, maze
     def go_left(prev):
-        loc, gem, maze, path = helper(prev)
-        path.append([prev.x-1, prev.y])
-        return Move(x=prev.x-1, y=prev.y, maze=maze, gem=gem, path=path)
+        loc, gem, maze = helper(prev)
+        return Move(x=prev.x-1, y=prev.y, maze=maze, gem=gem)
     def go_right(prev):
-        loc, gem, maze, path = helper(prev)
-        path.append([prev.x+1, prev.y])
-        return Move(x=prev.x+1, y=prev.y, maze=maze, gem=gem, path=path)
+        loc, gem, maze = helper(prev)
+        return Move(x=prev.x+1, y=prev.y, maze=maze, gem=gem)
     def go_up(prev):
-        loc, gem, maze, path = helper(prev)
-        path.append([prev.x, prev.y+1])
-        return Move(x=prev.x, y=prev.y+1, maze=maze, gem=gem, path=path)
+        loc, gem, maze = helper(prev)
+        return Move(x=prev.x, y=prev.y+1, maze=maze, gem=gem)
     def go_down(prev):
-        loc, gem, maze, path = helper(prev)
-        path.append([prev.x, prev.y-1])
-        return Move(x=prev.x, y=prev.y-1, maze=maze, gem=gem, path=path)
+        loc, gem, maze = helper(prev)
+        return Move(x=prev.x, y=prev.y-1, maze=maze, gem=gem)
     def valid(val):
         return val != '#' and val != 'X'
     def can_go_left(s):
         x = s.x - 1
         y = s.y
-        if x < 0:
-            return False
+        if x < 0: return False
         val = s.maze[y][x]
         return valid(val) and not (is_gem(val) and val == s.gem)
     def can_go_right(s):
         x = s.x + 1
         y = s.y
-        if x >= max_x:
-            return False
+        if x >= max_x: return False
         val = s.maze[y][x]
         return valid(val) and not (is_gem(val) and val == s.gem)
     def can_go_up(s):
         x = s.x
         y = s.y + 1
-        if y >= max_y:
-            return False
+        if y >= max_y: return False
         val = s.maze[y][x]
         return valid(val) and not (is_gem(val) and val == s.gem)
     def can_go_down(s):
         x = s.x
         y = s.y - 1
-        if y < 0:
-            return False
+        if y < 0: return False
         val = s.maze[y][x]
         return valid(val) and not (is_gem(val) and val == s.gem)
-    def is_goal(move):
-        return move.x == goal_x and move.y == goal_y
-    def pretty_print(move):
-        m = move.maze
-        for y in m:
-            print ''.join(y)
-            
-    # parse input
     for line in sys.stdin:
         max_y, max_x = line.rstrip().split()
         max_x = int(max_x)
         max_y = int(max_y)
-        break;
+        break
     for line in sys.stdin:
-        start_y, start_x = line.rstrip().split()
+        start_y, start_x = line.rstrip().split() # X and Y position for starting coords are backwards
         start_x = int(start_x)
         start_y = int(start_y)
-        break;
+        break
     maze = []
     for y, line in enumerate(sys.stdin):
         maze.append(list(line.rstrip()))
@@ -87,21 +68,15 @@ if __name__ == '__main__':
             goal_x = line.find('G')
             goal_y = y
     maze[start_y][start_x] = 'X'
-    first = Move(x=start_x, y=start_y, maze=maze, gem='', path=[[start_x, start_y]])
-    q = [first]
+    q = [Move(x=start_x, y=start_y, maze=maze, gem='')]
     while q:
         move = q.pop(0)
-        if is_goal(move):
+        if move.x == goal_x and move.y == goal_y: # reached goal!
             move.maze[move.y][move.x] = 'X'
-            pretty_print(move)
+            for y in move.maze: print ''.join(y)
             sys.exit(0)
-        if can_go_left(move):
-            q.append(go_left(move))
-        if can_go_right(move):
-            q.append(go_right(move))
-        if can_go_up(move):
-            q.append(go_up(move))
-        if can_go_down(move):
-            q.append(go_down(move))
+        if can_go_left(move): q.append(go_left(move))
+        if can_go_right(move): q.append(go_right(move))
+        if can_go_up(move): q.append(go_up(move))
+        if can_go_down(move): q.append(go_down(move))
     print "NO SOLUTION"
-
