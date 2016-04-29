edce73b8e0800f145a9805ebf4e2706a84f44cd3
Peter Lesslie <pclesslie@gmail.com>
2016-04-28 20:04:39 -0500

    Final line reduction

--- 04_glittering_mazes/solution.py
+++ 04_glittering_mazes/solution.py
@@ -16,20 +16,16 @@ if __name__ == '__main__':
     def go_up(prev): return Move(prev.x, prev.y+1, *helper(prev))
     def go_down(prev): return Move(prev.x, prev.y-1, *helper(prev))
     def can_go_left(s):
-        if s.x - 1 < 0: return False
-        val = s.maze[s.y][s.x - 1]
+        val = 'X' if s.x - 1 < 0 else s.maze[s.y][s.x - 1]
         return valid(val) and not (is_gem(val) and val == s.gem)
     def can_go_right(s):
-        if s.x + 1 >= max_x: return False
-        val = s.maze[s.y][s.x + 1]
+        val = 'X' if s.x + 1 >= max_x else s.maze[s.y][s.x + 1]
         return valid(val) and not (is_gem(val) and val == s.gem)
     def can_go_up(s):
-        if s.y + 1 >= max_y: return False
-        val = s.maze[s.y + 1][s.x]
+        val = 'X' if s.y + 1 >= max_y else s.maze[s.y + 1][s.x]
         return valid(val) and not (is_gem(val) and val == s.gem)
     def can_go_down(s):
-        if s.y - 1 < 0: return False
-        val = s.maze[s.y - 1][s.x]
+        val = 'X' if s.y - 1 < 0 else s.maze[s.y - 1][s.x]
         return valid(val) and not (is_gem(val) and val == s.gem)
     max_y, max_x = [int(x) for x in sys.stdin.readline().rstrip().split()]
     start_y, start_x = [int (x) for x in sys.stdin.readline().rstrip().split()] # X and Y position for starting coords are backwards
