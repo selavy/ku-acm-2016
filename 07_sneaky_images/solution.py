4118a9f47b25565ac61877b0e73d2c531fe9cae8
Peter Lesslie <pclesslie@gmail.com>
2016-04-29 20:13:30 -0500

    Sneaky images solution

--- 07_sneaky_images/solution.py
+++ 07_sneaky_images/solution.py
@@ -2,5 +2,14 @@
 
 import sys
 
-for line in sys.stdin:
-    print line
+output = ''
+cur = ''
+for val in sys.stdin.readline().rstrip().split():
+    if val[2] in ('0', '1'):
+        cur += val[2]
+        if len(cur) >= 8:
+            output += chr(int(cur, 2))
+            cur = ''
+    else:
+        cur = ''
+print output
